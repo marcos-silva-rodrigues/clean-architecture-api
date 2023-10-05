0f3c61907b30581c756359ede1b5972244b6145c
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-10-05 20:40:05 -0300

    test: validation email null

---
+++ src/entities/email-validation.spec.ts
@@ -0,0 +1,18 @@
+import { Email } from './email'
+
+describe('Email validation', () => {
+  test('should not accept null strings', () => {
+    const email = null
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should not accept empty strings', () => {
+    const email = ''
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should accept valid strings', () => {
+    const email = 'fulano@gmail.com'
+    expect(Email.validate(email)).toBeTruthy()
+  })
+})
