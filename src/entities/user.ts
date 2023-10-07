43dc71f78689d58d7efcb1748b0595ef7c83b4ba
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-10-07 16:52:58 -0300

    feat: creating valid user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -6,12 +6,19 @@ import { Name } from './name'
 import { type UserData } from './user-data'
 
 export class User {
+  private constructor (
+    public readonly name: Name,
+    public readonly email: Email
+  ) { }
+
   static create (userData: UserData): Either<InvalidEmailError | InvalidNameError, User> {
     const emailOrError = Email.create(userData.email)
     const nameOrError = Name.create(userData.name)
     if (emailOrError.isLeft()) return left(emailOrError.value)
     if (nameOrError.isLeft()) return left(nameOrError.value)
 
-    return right(new User())
+    const name = nameOrError.value
+    const email = emailOrError.value
+    return right(new User(name, email))
   }
 }
