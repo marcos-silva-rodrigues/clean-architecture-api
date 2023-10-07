409e94f2be569ee46fc381a65fa9f4ad42aa9322
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-10-07 16:32:08 -0300

    feat: create name value object and invalid name error

---
+++ src/entities/name.ts
@@ -0,0 +1,30 @@
+import { left, type Either, right } from '../shared/either'
+import { InvalidNameError } from './error/Invalid-name-error'
+
+export class Name {
+  public readonly value: string
+
+  private constructor (name: string) {
+    this.value = name
+  }
+
+  public static create (name: string): Either<InvalidNameError, Name> {
+    if (!Name.validate(name)) {
+      return left(new InvalidNameError())
+    }
+
+    return right(new Name(name))
+  }
+
+  public static validate (name: string | null): boolean {
+    if (name === null) {
+      return false
+    }
+
+    if (name.trim().length < 2) {
+      return false
+    }
+
+    return true
+  }
+}
