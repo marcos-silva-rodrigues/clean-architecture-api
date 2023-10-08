eb413fe5e4781a491433cb14933670b2e9f27d21
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-10-08 17:05:09 -0300

    test: should not add user with name invalid

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,3 +1,4 @@
+import { InvalidNameError } from '../../entities/error/Invalid-name-error'
 import { InvalidEmailError } from '../../entities/error/invalid-email-error'
 import { type UserData } from '../../entities/user-data'
 import { type UserRepository } from './ports/users-repository'
@@ -35,5 +36,21 @@ describe('Register user on mailing list use case', () => {
     const user = await repo.findUserByEmail(invalidEmail)
     expect(user).toBeNull()
     expect(response.value).toEqual(new InvalidEmailError())
+  })
+
+  test('should not add user with name invalid', async () => {
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const usecase = new RegisterUserOnMailingList(repo)
+    const invalidName = ''
+    const email = 'any@gmail.com'
+    const response = await usecase.registerUserOnMailingList({
+      name: invalidName,
+      email
+    })
+
+    const user = await repo.findUserByEmail(email)
+    expect(user).toBeNull()
+    expect(response.value).toEqual(new InvalidNameError())
   })
 })
