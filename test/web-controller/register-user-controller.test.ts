beff461cacfd88d0f81ef8755c65281532e229fd
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-10-13 07:32:53 -0300

    test: bad request when pass invalid data

--- test/web-controller/register-user-controller.test.ts
+++ test/web-controller/register-user-controller.test.ts
@@ -1,4 +1,5 @@
 import { type UserData } from '@/entities'
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
 import { type HttpRequest } from '@/web-controller/ports'
 import { type HttpResponse } from '@/web-controller/ports/http-response'
@@ -23,5 +24,43 @@ describe('Register user with controller', () => {
 
     expect(response.statusCode).toEqual(201)
     expect(response.body).toEqual(request.body)
+  })
+
+  test('should return status code 400 when request contains invalid name', async () => {
+    const request: HttpRequest = {
+      body: {
+        name: 'A',
+        email: 'any@gmail.com'
+      }
+    }
+
+    const users: UserData[] = []
+
+    const repo = new InMemoryUserRepository(users)
+    const usecase = new RegisterUserOnMailingList(repo)
+    const controller = new RegisterUserController(usecase)
+    const response: HttpResponse = await controller.handle(request)
+
+    expect(response.statusCode).toEqual(400)
+    expect(response.body).toBeInstanceOf(InvalidNameError)
+  })
+
+  test('should return status code 400 when request contains invalid email', async () => {
+    const request: HttpRequest = {
+      body: {
+        name: 'Any name',
+        email: 'any_gmail.com'
+      }
+    }
+
+    const users: UserData[] = []
+
+    const repo = new InMemoryUserRepository(users)
+    const usecase = new RegisterUserOnMailingList(repo)
+    const controller = new RegisterUserController(usecase)
+    const response: HttpResponse = await controller.handle(request)
+
+    expect(response.statusCode).toEqual(400)
+    expect(response.body).toBeInstanceOf(InvalidEmailError)
   })
 })
